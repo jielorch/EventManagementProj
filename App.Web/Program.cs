f07f5ef93e231dcfd25164abb4e3aea5e541edb3
rain llorca <rainllorca@gmail.com>
2026-03-07 10:16:06 +0800

    updates

--- App.Web/Program.cs
+++ App.Web/Program.cs
@@ -62,26 +62,28 @@ app.MapControllers();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // In Production (MonsterASP), if no API or File is found, send index.html
-   
-        app.MapFallback(async context =>
-        {
-            var path = context.Request.Path.Value?.Trim('/') ?? "";
-
-            // Check for the subfolder file (e.g. wwwroot/browser/about/index.html)
-            var subfolderIndex = Path.Combine(browserPath, path, "index.html");
-
-            if (!string.IsNullOrEmpty(path) && File.Exists(subfolderIndex))
-            {
-                await context.Response.SendFileAsync(subfolderIndex);
-            }
-            else
-            {
-                // Fallback to the main index inside /browser/
-                await context.Response.SendFileAsync(Path.Combine(browserPath, "index.html"));
-            }
-        }); 
+    
 }
+
+// In Production (MonsterASP), if no API or File is found, send index.html
+
+app.MapFallback(async context =>
+{
+    var path = context.Request.Path.Value?.Trim('/') ?? "";
+
+    // Check for the subfolder file (e.g. wwwroot/browser/about/index.html)
+    var subfolderIndex = Path.Combine(browserPath, path, "index.html");
+
+    if (!string.IsNullOrEmpty(path) && File.Exists(subfolderIndex))
+    {
+        await context.Response.SendFileAsync(subfolderIndex);
+    }
+    else
+    {
+        // Fallback to the main index inside /browser/
+        await context.Response.SendFileAsync(Path.Combine(browserPath, "index.html"));
+    }
+});
 //else
 //{
 //    app.UseSpa(spa =>
